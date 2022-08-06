e623cdabe1f6abf168dd01f0879565fa7f9e38a3
Ernest Bolot <ernstin@gmail.com>
2022-08-06 14:53:05 -0500

    students arraylist review

---
+++ collections-review/src/main/java/Student.java
@@ -0,0 +1,44 @@
+public class Student {
+    String name;
+    int age;
+
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
