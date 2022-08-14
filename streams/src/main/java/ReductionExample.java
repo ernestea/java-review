74e7319812720f92eaf0705ec2421dcc2f25cba0
Ernest.Kozhokmatov <ernstea@github.com>
2022-08-14 13:27:33 -0500

    Fibonacci task using stream iterate method

--- streams/src/main/java/ReductionExample.java
+++ streams/src/main/java/ReductionExample.java
@@ -1,3 +1,5 @@
+import dish.Dish;
+import dish.DishData;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -6,6 +8,22 @@ import java.util.Optional;
 
 public class ReductionExample {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    @Test
+    public void dishTask(){//Count number of dishes in a stream using map and reduce methods
+        Optional<Integer> result = DishData.readAll().stream()
+                .map(dish -> 1)
+                .reduce(Integer::sum);
+        System.out.println(result);
+
+        int result2 = DishData.readAll().stream()
+                .map(dish -> 1)
+                .reduce(0, Integer::sum);
+        System.out.println(result2);
+
+        long result3 = DishData.readAll().stream().count();
+        System.out.println(result3);
+    }
 
     @Test
     public void sum() {
